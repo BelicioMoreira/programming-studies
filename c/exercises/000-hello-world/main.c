18174ec2693c3d43ecb39c884f3fe5705ca1929f
Belicio Moreira <belicio.moreira@gmail.com>
2026-04-26 19:23:53 -0300

    add exercise 000: hello world in C

---
+++ c/exercises/000-hello-world/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {]
+    
+    // This is my firt program!
+    /*
+    This
+    is
+    also
+    a
+    comment
+    */
+
+    printf ("Hello, World!");
+    return 0;
+}
